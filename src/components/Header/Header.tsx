47647a2e49d3555c4af42c13f7348a73be5e7aeb
somadina6 <somadina6@gmail.com>
2023-12-27 05:16:26 -0400

    Update navigation links and logo size

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,28 +2,33 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+const navLinks = ["HOME", "APPOINTMENT", "ABOUT", "PRODUCTS", "CONTACT"];
+
+const navLinksURL: { [key: string]: string } = {
+  HOME: "/",
+  APPOINTMENT: "/appointment",
+  ABOUT: "/about",
+  PRODUCTS: "/products",
+  CONTACT: "/contact",
+};
+
 const Header: React.FC = () => {
   return (
     <header className="w-full flex justify-between px-8">
-      <ul className="w-full md:w-2/3 flex justify-between p-4">
-        <li>
-          <Link href="/" className="text-md font-bold hover:border-b-green-500">
-            HOME
-          </Link>
-        </li>
-        <li>
-          <Link href="/" className="text-md font-bold hover:border-b-green-500">
-            ABOUT
-          </Link>
-        </li>
-        <li>
-          <Link href="/" className="text-md font-bold hover:border-b-green-500">
-            PRODUCTS
-          </Link>
-        </li>
+      <ul className="w-full md:w-3/5 flex justify-between p-4">
+        {navLinks.map((link) => {
+          return (
+            <li
+              key={link}
+              className="text-lg hover:border-b-black hover:border-b-2"
+            >
+              <Link href={navLinksURL[link]}>{link}</Link>
+            </li>
+          );
+        })}
       </ul>
 
-      <Image src="/next.svg" alt="Next Image" width={20} height={20} />
+      <Image src="/next.svg" alt="Next Image" width={100} height={100} />
     </header>
   );
 };
