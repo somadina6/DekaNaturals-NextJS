56ecd92b125b6129f12850fc90ff109bb73f721f
somadina6 <somadina6@gmail.com>
2023-12-30 04:14:13 -0400

    Update global CSS and Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,29 +6,29 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className=" h-24 w-full flex flex-col items-center ">
-      <div className="w-1/2 flex items-center justify-between">
+      <div className="w-1/2 flex items-center justify-between mb-0">
         <Link href="/">
           <span className="flex items-center py-4">
             <FaRegCopyright />
-            <p className="ml-2">Deka Naturals</p>
+            <p className="ml-1">Deka Naturals</p>
           </span>
         </Link>
 
         <Link href="/">
           <span className="flex items-center py-4">
             <FaFileContract />
-            <p className="ml-2">Terms & Conditions</p>
+            <p className="ml-1">Terms & Conditions</p>
           </span>
         </Link>
 
         <Link href="/">
           <span className="flex items-center py-4">
             <MdOutlinePrivacyTip />
-            <p className="ml-2">Privacy Policy</p>
+            <p className="ml-1">Privacy Policy</p>
           </span>
         </Link>
       </div>
-      <Link className="mt-1" href="/">
+      <Link className="mt-0" href="/">
         <Image src="/logo.png" alt="Next Image" width={120} height={80} />
       </Link>
     </footer>
