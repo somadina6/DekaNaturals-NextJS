c2b908cba8d08d4356d02edc284ddbe465bd9628
somadina6 <somadina6@gmail.com>
2024-01-06 00:50:23 -0400

    Update footer and social media icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,40 +1,46 @@
 import Link from "next/link";
 import { FaRegCopyright, FaFileContract } from "react-icons/fa";
 import { MdOutlinePrivacyTip } from "react-icons/md";
-import Image from "next/image";
-import Socials from "../Socials/Socials";
+import { TiSocialFacebook, TiSocialInstagram } from "react-icons/ti";
 
 const Footer = () => {
   return (
-    <footer className=" h-24 w-full flex flex-col items-center ">
-      <div className="w-1/2 flex flex-col lg:flex-row items-center justify-between mb-0">
-        <Link href="/">
-          <span className="flex items-center py-4">
-            <FaRegCopyright />
-            <p className="ml-1">Deka Naturals</p>
-          </span>
-        </Link>
+    <footer>
+      <section className=" h-24 w-full md:w-1/2 mx-auto flex items-center justify-between mt-5">
+        <div className="flex flex-col items-start justify-between">
+          <p className="ml-1 pb-2">Important Links</p>
+          <Link href="/" className="hoverbold">
+            <span className="flex items-center pb-4">
+              <FaFileContract />
+              <p className="ml-1">Terms & Conditions</p>
+            </span>
+          </Link>
 
-        <Link href="/">
-          <span className="flex items-center py-4">
-            <FaFileContract />
-            <p className="ml-1">Terms & Conditions</p>
-          </span>
-        </Link>
+          <Link href="/" className="hoverbold">
+            <span className="flex items-center pb-4">
+              <MdOutlinePrivacyTip />
+              <p className="ml-1">Privacy Policy</p>
+            </span>
+          </Link>
+        </div>
 
-        <Link href="/">
-          <span className="flex items-center py-4">
-            <MdOutlinePrivacyTip />
-            <p className="ml-1">Privacy Policy</p>
-          </span>
-        </Link>
-      </div>
-      <div className="flex justify-between items-center px-2 py-1 w-1/4">
-        <Link className="mt-0" href="/">
-          <Image src="/logo.png" alt="Next Image" width={120} height={80} />
-        </Link>
-        <Socials />
-      </div>
+        <div className="  flex flex-col justify-between items-start">
+          <h5 className="ml-1 pb-2">Connect with us:</h5>
+          <div className="flex items-center hoverbold pb-4">
+            <TiSocialFacebook />
+            <p className="ml-1">Facebook</p>
+          </div>
+          <div className="flex items-center hoverbold pb-4">
+            <TiSocialInstagram />
+            <p className="ml-1">Instagram</p>
+          </div>
+        </div>
+      </section>
+      <section className="w-full h-4 flex items-center justify-center">
+        <p className="text-sm text-gray-600">
+          &copy; {new Date().getFullYear()} DekaNaturals. All rights reserved.
+        </p>
+      </section>
     </footer>
   );
 };
--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -2,14 +2,15 @@ import { TiSocialFacebook, TiSocialInstagram } from "react-icons/ti";
 
 const Socials: React.FC = () => {
   return (
-    <div className="flex flex-col justify-normal items-start">
+    <div className="flex flex-col justify-evenly items-center">
+      <h5>Connect with us:</h5>
       <div className="flex items-center ">
         <TiSocialFacebook />
-        <p className="ml-1">Deka Naturals</p>
+        <p className="ml-1">Facebook</p>
       </div>
       <div className="flex items-center">
         <TiSocialInstagram />
-        <p className="ml-1">dekanaturals</p>
+        <p className="ml-1">Instagram</p>
       </div>
     </div>
   );
